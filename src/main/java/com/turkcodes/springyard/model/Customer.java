0bf0fdaf8780c14bdd0c86ca52124d236cdc69c8
DavidTurkGT <dlturk747@gmail.com>
2017-08-03 13:30:39 -0400

    Added customer model

---
+++ src/main/java/com/turkcodes/springyard/model/Customer.java
@@ -0,0 +1,62 @@
+package com.turkcodes.springyard.model;
+
+/**
+ * Created by David Turk on 8/3/17.
+ */
+public class Customer {
+    private int id;
+    private String firstname;
+    private String lastname;
+    private String phone;
+    private String email;
+
+    public Customer() {
+    }
+
+    public Customer(String firstname, String lastname, String phone, String email) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}

2743a2f95b7f8ef25956423ae62e31b495ec4723
DavidTurkGT <dlturk747@gmail.com>
2017-08-03 13:45:49 -0400

    CustomerServiceImpl is atomically transactional and passes through to CustomerRepository

--- src/main/java/com/turkcodes/springyard/service/CustomerServiceImpl.java
+++ src/main/java/com/turkcodes/springyard/service/CustomerServiceImpl.java
@@ -4,6 +4,7 @@ import com.turkcodes.springyard.model.Customer;
 import com.turkcodes.springyard.repository.CustomerRepository;
 import com.turkcodes.springyard.repository.CustomerRepositoryImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,28 +15,30 @@ public class CustomerServiceImpl implements CustomerService {
     @Autowired
     CustomerRepository customerRepository;
 
+    @Transactional
     @Override
     public void addCustomer(Customer customer) {
-
+        customerRepository.addCustomer(customer);
     }
 
     @Override
     public List<Customer> getAllCustomers() {
-        return null;
+        return customerRepository.getAllCustomers();
     }
-
     @Override
     public Customer getCustomerById(int id) {
-        return null;
+        return customerRepository.getCustomerById(id);
     }
 
+    @Transactional
     @Override
     public void updateCustomer(Customer customer) {
-
+        customerRepository.updateCustomer(customer);
     }
 
+    @Transactional
     @Override
     public void deleteCustomer(int id) {
-
+        customerRepository.deleteCustomer(id);
     }
 }

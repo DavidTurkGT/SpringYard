a9a0cafad793ed3b6e5c3864f9d09afe18b3cee7
DavidTurkGT <dlturk747@gmail.com>
2017-08-03 13:48:23 -0400

    Added framework for CustomerService unit tests

---
+++ src/test/java/com/turkcodes/springyard/service/CustomerServiceTest.java
@@ -0,0 +1,24 @@
+package com.turkcodes.springyard.service;
+
+import org.junit.Test;
+
+/**
+ * Created by David Turk on 8/3/17.
+ */
+public class CustomerServiceTest {
+
+    @Test
+    public void testAddGet(){
+
+    }
+
+    @Test
+    public void testUpdate(){
+
+    }
+
+    @Test
+    public void testDelete(){
+        
+    }
+}

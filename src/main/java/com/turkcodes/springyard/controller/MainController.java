aeaf447f6d559922c1a4d938e8d8e31ccae1e3d0
DavidTurkGT <dlturk747@gmail.com>
2017-08-04 13:49:22 -0400

    Home page pulls all customers from the database and displays them

--- src/main/java/com/turkcodes/springyard/controller/MainController.java
+++ src/main/java/com/turkcodes/springyard/controller/MainController.java
@@ -1,8 +1,14 @@
 package com.turkcodes.springyard.controller;
 
+import com.turkcodes.springyard.model.Customer;
+import com.turkcodes.springyard.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 /**
  * Created by David Turk on 8/4/17.
@@ -10,8 +16,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class MainController {
 
+    @Autowired
+    CustomerService customerService;
+
     @RequestMapping("/")
-    public String getHome(){
+    public String getHome(Model model){
+        //Retrieve all Customers from the database
+        List<Customer> customers = customerService.getAllCustomers();
+        model.addAttribute("customers", customers);
         return "index";
     }
 
